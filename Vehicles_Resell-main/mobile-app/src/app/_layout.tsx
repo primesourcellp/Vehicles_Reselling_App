72c0f538d01325535be40070ba97cf0c18e8bdee
Pragathikumaresan7 <pragathikumaresan24@gmail.com>
2026-09-03 17:28:46 +0530

    ..

--- Vehicles_Resell-main/mobile-app/src/app/_layout.tsx
+++ Vehicles_Resell-main/mobile-app/src/app/_layout.tsx
@@ -1,19 +1,13 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
-import { useColorScheme } from 'react-native';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-        <Stack.Screen name="login" />
-        <Stack.Screen name="register" />
-        <Stack.Screen name="verify" />
-        <Stack.Screen name="(tabs)" />
-      </Stack>
-    </ThemeProvider>
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="login" />
+      <Stack.Screen name="register" />
+      <Stack.Screen name="verify" />
+      <Stack.Screen name="(tabs)" />
+    </Stack>
   );
 }
